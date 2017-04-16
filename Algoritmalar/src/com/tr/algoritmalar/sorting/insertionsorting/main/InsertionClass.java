5d7366034fc42ba0fc5e5af65ee0a4f1a8ee14e2
utku <utku.0865@gmail.com>
2017-04-16 22:03:37 +0300

    Insertion sort sınıfı ve test sınıfı eklendi

--- Algoritmalar/src/com/tr/algoritmalar/sorting/insertionsorting/main/InsertionClass.java
+++ Algoritmalar/src/com/tr/algoritmalar/sorting/insertionsorting/main/InsertionClass.java
@@ -1,6 +1,32 @@
 package com.tr.algoritmalar.sorting.insertionsorting.main;
 
 public class InsertionClass {
-	//Insertıon Sort
 
+	public int[] insertionSort(int[] dizi) {
+
+		int key;
+		int i;
+		for (int j = 1; j < dizi.length; j++) {
+			key = dizi[ j ];
+			i = j - 1;
+			while (i >= 0 && key < dizi[i]) {
+				dizi[i + 1] = dizi[i];
+				i--;
+			}
+			dizi[i + 1] = key;
+		}
+
+		return dizi;
+	}
+
+	public int [] randomIntegerArrayGenerate(int size) {
+
+		int [] array = new int [size];
+		for(int i = 0; i<size;i++ ) {
+			
+			array[i] = (int)(Math.random()*1000);
+		}
+		return array;
+		
+	}
 }
---
+++ Algoritmalar/src/com/tr/algoritmalar/sorting/insertionsorting/test/TestInsertionSort.java
@@ -0,0 +1,61 @@
+package com.tr.algoritmalar.sorting.insertionsorting.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tr.algoritmalar.sorting.insertionsorting.main.InsertionClass;
+
+public class TestInsertionSort {
+
+	InsertionClass sort;
+
+	@Before
+	public void setUp() {
+		sort = new InsertionClass();
+
+	}
+
+	@Test
+	public void testInsertSort() {
+
+		int[] unsortedArray = { 3, 2, 3, 4, 6, 1, 5 };
+		int[] sortedArray = { 1, 2, 3, 3, 4, 5, 6 };
+
+		Assert.assertArrayEquals(sort.insertionSort(unsortedArray), sortedArray);
+
+	}
+
+	@Test
+	public void randomvalueTest() {
+
+		int diziBoyutu = 20;
+		boolean bool = true;
+		int[] dizi = sort.randomIntegerArrayGenerate(diziBoyutu);
+		dizi = sort.insertionSort(dizi);
+		for (int i = 0; i < dizi.length; i++) {
+			if (i + 1 < dizi.length && dizi[i] > dizi[i + 1]) {
+				bool = false;
+			}
+			Assert.assertTrue(bool);
+		}
+	}
+
+	@Test
+	 public void insertionSortTimeTest() {
+	
+	 int diziBoyutu;
+	 int[] dizi =null;
+	
+	 for( diziBoyutu=10000; diziBoyutu<10_000_000;diziBoyutu*=10 ) {
+	 dizi = sort.randomIntegerArrayGenerate(diziBoyutu);
+	 long startTime = System.currentTimeMillis();
+	 sort.insertionSort(dizi);
+	 long endTime = System.currentTimeMillis();
+	 long estimatedTime = endTime - startTime; 
+	 double seconds = (double)estimatedTime/1000; 
+	 System.out.println(" Eleman Sayısı "+ diziBoyutu + " süre " + seconds );
+	 }
+	 }
+
+}
